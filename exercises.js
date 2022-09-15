7e8eeee086cadd7bba1818d075931074782b3013
axelhagerback <axel.hagerback@gmail.com>
2022-09-15 12:03:16 +0200

    exercise 1 done

--- exercises.js
+++ exercises.js
@@ -1,9 +1,20 @@
-console.log('I am ALIVE');
-
 function exercise1() {
-    console.log('first exercise');
-    var textbox1value = document.getElementById('txt1').value;
-    var textbox2value = document.getElementById('txt2').value;
-    console.log(textbox1value + ', ' + textbox2value);
-    console.log(`${textbox1value}, ${textbox2value} (${textbox1value.length})`);
+    var textboxValue = document.getElementById('txt').value;
+    const words = textboxValue.split(' ');
+    
+    console.log(words.length);
+    let i = 0;
+
+
+    while(i < words.length) {
+        console.log(`${words[i]} (${words[i].length})`);
+        i++;
+    }
+
+    
 }
+
+
+
+
+
