fc75af3912fdb66c6374b8f4c3a0b9fc6f171fc4
axelhagerback <axel.hagerback@gmail.com>
2022-09-15 13:01:12 +0200

    exercise 2 done

--- exercises.js
+++ exercises.js
@@ -15,6 +15,34 @@ function exercise1() {
 }
 
 
+function exercise2() {
+    var textboxValue = document.getElementById('txt').value;
+    words = textboxValue.split(' ');
 
+
+    
+    const sortedWords = words.sort((a, b) => a.length - b.length);
+    
+    
+    let i = 0;
+    
+    while(i < sortedWords.length) {
+        console.log(`${sortedWords[i]}`);
+        i++;
+    }
+
+    reversedWords = words.reverse();
+    let x = 0;
+    while(x < reversedWords.length) {
+        console.log(`${reversedWords[x]}`);
+        x++;
+    }
+}
+
+
+function exercise3() {
+    var textboxValue = document.getElementById('txt').value;
+    words = textboxValue.split(' ');
+}
 
 
